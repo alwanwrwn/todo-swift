92fa622d6f185ebeb99dbf935180fa545ac64931
Alwan Wirawan <alwanwrwn@student.uns.ac.id>
2023-09-28 09:56:34 +0700

    Initial Commit

---
+++ todo-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-ios
+//
+//  Created by Alwan Wirawan on 28/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
